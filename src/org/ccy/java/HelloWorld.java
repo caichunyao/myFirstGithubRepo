5fa944d795d0a14aabe65a4e5e13dcd8f1e82a5a
caichunyao <caichuny@cn.ibm.com>
2017-06-20 19:58:17 +0800

    Update HelloWorld.java

--- src/org/ccy/java/HelloWorld.java
+++ src/org/ccy/java/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//add comments here for test
 		System.out.println("*****Hello world*****");
 	}
 
